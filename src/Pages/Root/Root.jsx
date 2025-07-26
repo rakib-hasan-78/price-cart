c74a8d29b348f907e0870096f145e68066024bd1
rakib hasan <rakibwebdev78@gmail.com>
2025-07-27 04:47:09 +0600

    headrooom bug rresolved

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Outlet} from 'react-router-dom';
 import Header from './../../Components/Header/Header';
-
+import Headroom from 'react-headroom';
 
 const Root = () => {
     
     return (
         <div>
             <main className='main'>
+            <Headroom>
             <Header />
+            </Headroom>
             <Outlet />
             </main>
         </div>
