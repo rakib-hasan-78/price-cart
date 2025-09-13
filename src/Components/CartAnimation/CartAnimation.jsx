4451185730d6fd2f20b062c61b3134cf1d5f0587
rakib hasan <rakibwebdev78@gmail.com>
2025-09-14 04:32:54 +0600

    wishlist handled

--- src/Components/CartAnimation/CartAnimation.jsx
+++ src/Components/CartAnimation/CartAnimation.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { useNavigate } from 'react-router-dom';
 
-const CartAnimation = () => {
+const CartAnimation = ({tabs}) => {
     const navigate = useNavigate();
 
     const productHubHandler = ()=>{
@@ -12,7 +12,17 @@ const CartAnimation = () => {
     return (
     <>
         <div className='w-full flex items-center justify-center'>
-            <h2 className='text-bg hero-title'>empty product !</h2>
+            <h2 className='text-bg hero-title capitalize'>
+                {
+                    tabs==='cart'? 
+                    'no items in cart !' :
+                    tabs === 'wishlist' && 
+                    (
+                        'no items in wishlist'
+                    )
+                }
+               
+            </h2>
         </div>
         <DotLottieReact
         src="https://lottie.host/fea3b89a-07cb-444c-bda1-c825cac4690a/PX7UPqmx1S.lottie"
--- src/Components/SelectedProduct/SelectedProduct.jsx
+++ src/Components/SelectedProduct/SelectedProduct.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import SelectedCart from './../SelectedCart/SelectedCart';
 import CartAnimation from './../CartAnimation/CartAnimation';
+import { useProduct } from '../../Utilities/Hooks/CustomContext/CustomContext';
 
 const SelectedProduct = ({tabs}) => {
-
+    const {cart, wishList} =useProduct();
     return (
         <div className='w-full p-5 h-auto flex flex-wrap xxs:flex-col sm:flex-row lg:flex-col items-center justify-center sm:space-x-3 xxs:space-y-4'>
-            <CartAnimation />
-            {/* <SelectedCart tabs={tabs} /> */}
+        {
+            tabs === 'cart'&&(
+                cart.length > 0 ? 
+                cart?.map(item=>(
+                    <SelectedCart 
+                    key={item.product_id}
+                     item={item}
+                      tabs={tabs} />
+                )) :
+            <CartAnimation tabs={tabs} />
+
+            )
+        }
+        {
+            tabs === 'wishlist' && (
+                wishList.length > 0 ?
+                wishList?.map(item=>(
+                    <SelectedCart
+                    key={item.product_id}
+                    item={item}
+                    tabs={tabs}
+                     />
+                )) :
+                <CartAnimation tabs={tabs} />
+            )
+        }
         </div>
     );
 };
