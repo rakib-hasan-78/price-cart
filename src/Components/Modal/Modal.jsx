121bd8af9b907d8c99224aca0a292515938a9ea9
rakib hasan <rakibwebdev78@gmail.com>
2025-11-21 23:30:15 +0600

    data sent done...

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useProduct } from "../../Utilities/Hooks/CustomContext/CustomContext";
+import SuccessAnimation from './../SuccessAnimation/SuccessAnimation';
+import Line from '../../assets/Line 2.png';
 
 const Modal = ({cName}) => {
+  const {totals} = useProduct()
   return (
     <div className="flex flex-col items-center justify-center">
       {/* 🔘 Button to open modal */}
@@ -12,18 +16,24 @@ const Modal = ({cName}) => {
       </button>
 
       {/* 💡 Modal */}
-      <dialog id="my_modal_3" className="modal bg-violet-600">
-        <div className="modal-box relative bg-violet-600">
+      <dialog id="my_modal_3" className="modal secondary-bg-gradient-nav">
+        <div className="modal-box relative">
+          
+
           {/* Close button (X) */}
           <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-black/25 hover:text-black">
               ✕
             </button>
           </form>
-
+          <div className="w-full flex flex-col items-center">
           {/* Modal content */}
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click on ✕ button to close.</p>
+          <SuccessAnimation />
+          <h3 className="font-bold xxs:text-sm md:text-base lg:text-lg xl:text-2xl pb-4 text-black">Payment Successfully</h3>
+          <img src={Line} alt="border-img" />
+          <p className="py-4 text-black/25">Thanks For Purchasing</p>
+          <p className="text-black/25">Total:{totals.grandTotal.toFixed(2)}</p>
+          </div>
         </div>
       </dialog>
     </div>
---
+++ src/Components/SuccessAnimation/SuccessAnimation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const SuccessAnimation = () => {
+    return (
+        <div className='flex items-center justify-center w-52'>
+            <DotLottieReact
+            src="https://lottie.host/4afab09e-6448-4845-a36c-88f0a910632c/5Zgpqv1ADK.lottie"
+            loop
+            autoplay
+            />
+        </div>
+    );
+};
+
+export default SuccessAnimation;
+
+
+
